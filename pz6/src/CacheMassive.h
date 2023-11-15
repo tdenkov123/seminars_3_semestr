59c05e6951eb42a63a8a99f0b56806688cfc8db8
tdenkov123 <71324191+tdenkov123@users.noreply.github.com>
2023-11-15 08:22:31 +0300

    working variatn (no sort yet)

--- pz6/src/CacheMassive.h
+++ pz6/src/CacheMassive.h
@@ -4,33 +4,24 @@
 
 using namespace std;
 
-template <typename T>
-class CacheMassive {
+template <typename T> class CacheMassive {
     private:
         struct Node {
             Node* next;
-            Node* prev;
             T data;
             int id, accessCount;
-            Node(T p_data, Node* p_prev, Node* p_next, int p_id) {
-                T data = p_data;
-                Node* prev = p_prev;
-                Node* next = p_next;
-                int id = p_id;
-                int accessCount = 0;
-            }
+            Node(T p_data, int p_id) {
+                data = p_data;
+                next = nullptr;
+                id = p_id;
+                accessCount = 0;
+            } 
         } *head;
-        int last_id = 0;
         int size;
     public:
-        CacheMassive(T p_data) {
-            head = new Node(p_data, nullptr, nullptr, last_id);
-            last_id++;
-            size = 1;
-        };
+        CacheMassive() : head(nullptr), size(0) {}
         CacheMassive(CacheMassive& p_mas) {
             head = p_mas.head;
-            last_id = p_mas.last_id;
             size = p_mas.size;
         }
         ~CacheMassive() {
@@ -42,47 +33,71 @@ class CacheMassive {
             }
         }
 
-        void print() {
-            if (head) {
-                Node* tmp = head;
-                do {
-                cout << tmp->id << ' ' << tmp->accessCount << ' ' << tmp->data << '\n';
+        void raiseAccessCount(int p_id) {
+            Node* tmp = head;
+            if (p_id == tmp->id) {
                 tmp->accessCount++;
-                } while (tmp->next);
             }
-        }
-
-        T getElem(int id) {
-            Node* tmp = head;
-            if (id >= last_id || id < 0) throw out_of_range("std::out_of_range::id");
-            while (tmp->id != id) tmp = tmp->next;
-            return tmp->id;
-            call_sorting();
-        }
-
-        int getSize() {return size;}
-
-        int getLastId() {return last_id;}
-        
-        void push_back(T elem) {
-            Node* tmp = head;
-            while (tmp->next) {
+            while (tmp->next != nullptr) {
                 tmp = tmp->next;
-            }
-            tmp->next = new Node(elem, tmp, nullptr, last_id);
-            last_id++;
-            size++;
-        }
-
-        void call_soorting() {
-            Node* tmp = head;
-            bool flag = true;
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size - i - 1; j++) {
-                    Node* tmp_next = tmp->next;
-                    if (tmp->accessCount < tmp_next->accessCount) swap(&tmp, &tmp_next);
+                if (p_id == tmp->id) {
+                    tmp->accessCount++;
                 }
             }
         }
-        
+
+        int getSize() const {return size;}
+
+        Node* getElemPointer(int p_id) {
+            Node* tmp = head;
+            if (p_id == tmp->id) return tmp;
+            while (tmp->next != nullptr) {
+                tmp = tmp->next;
+                if (p_id == tmp->id) return tmp;
+            }
+            return nullptr;
+        }
+
+        void push_back(T elem, int p_id) {
+            Node* tmp = head;
+            Node* newNode = new Node(elem, p_id);
+            if (head) {
+                for (int i = 0; i < size; i++) {
+                    if (!tmp->next) break;
+                    tmp = tmp->next;
+                }
+                tmp->next = newNode;
+            } else head = newNode;
+            size++;
+        }
+
+        void print() {
+            Node* tmp = head;
+            while (tmp) {
+                cout << "{" << tmp->id << ' ' << tmp->accessCount << ' ' << tmp->data << "} ";
+                tmp = tmp->next;
+            }
+            cout << '\n';
+        }
+
+        void sort() {
+            Node* tmp = head;
+
+        }
+
+        string search(T elem) {
+            Node* tmp = head;
+            bool flag = false;
+            for (int i = 0; i < size; i++) {
+                if (tmp->data == elem) flag = true;
+            }
+            if (flag) return "YES\n";
+            else return "NO\n";
+        }
+
+        void update(int p_id, T elem) {
+            Node* tmp = getElemPointer(p_id);
+            if (tmp == nullptr) throw invalid_argument("std::invalid_argument");
+
+        }
 };
--- pz6/src/main.cpp
+++ pz6/src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+#include "CacheMassive.h"
+
+using namespace std;
+
 int main(int argc, char** argv) {
-    
+    CacheMassive<int> m;
+    m.push_back(10, 1);
+    m.push_back(20, 2);
+    m.push_back(5, 3);
+    m.push_back(100, 4);
+
+    if (m.getSize()) cout << m.getSize();
+    else cout << ';';
+ 
+    cout << m.getElem(4) << '\n';
+    m.print();
 
     return 0;
 }
